59460ae87b5eb5a1c5896d7f7f49fa6fc8d8e762
Brian Weaver <bdrums@gmail.com>
2017-11-04 15:13:59 -0400

    feat(extension): Make it all work!

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,34 @@
 "use strict";
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode"
 import * as fs from "fs"
 import * as path from "path"
 import * as mkdirp from "mkdirp"
 import FlipFlop from "./flip-flop"
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log("Congratulations, your extension 'rails-flip-flop' is now active!")
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("extension.railsFlipFlop", () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage("We gonna flip 'dem flops!")
-
-    let editor = vscode.window.activeTextEditor
+  const disposable = vscode.commands.registerCommand("extension.railsFlipFlop", () => {
+    const editor = vscode.window.activeTextEditor
     if (!editor) return
 
-    let document = editor.document
-    let fileName: string = document.fileName
-    let specType: string = getSpecType()
-    let flipFlop: FlipFlop = new FlipFlop(fileName, specType)
-    let relatedFilePath: string = flipFlop.getRelatedFilePath()
-  });
+    const fileName: string = editor.document.fileName
+    const specType: string = getSpecType()
+    const flipFlop: FlipFlop = new FlipFlop(fileName, specType)
+    const relatedFilePath: string = flipFlop.getRelatedFilePath()
+
+    if (fs.existsSync(relatedFilePath)) {
+      openFile(relatedFilePath)
+    } else {
+      prompt(vscode.workspace.asRelativePath(relatedFilePath), function() {
+        mkdirp.sync(path.dirname(relatedFilePath))
+        fs.closeSync(fs.openSync(relatedFilePath, 'w'))
+        openFile(relatedFilePath)
+      })
+    }
+  })
 
   context.subscriptions.push(disposable)
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
 
@@ -49,10 +40,10 @@ function getSpecType() {
 }
 
 function dirExists(dir) {
-  let rootPath = vscode.workspace.rootPath
+  const rootPath = vscode.workspace.rootPath
 
   try {
-    fs.statSync(path.resolve(`${rootPath}/${dir}`)).isDirectory()
+    return fs.statSync(path.resolve(`${rootPath}/${dir}`)).isDirectory()
   } catch (e) {
     if (e.code === 'ENOENT') {
       return false
@@ -60,4 +51,19 @@ function dirExists(dir) {
       throw e
     }
   }
+}
+
+function openFile(fileName) {
+	vscode.workspace
+		.openTextDocument(fileName)
+		.then(vscode.window.showTextDocument)
+}
+
+function prompt(fileName, cb) {
+	const options = { placeHolder: `Create ${fileName}?` }
+  vscode.window
+    .showQuickPick(["Yes", "No"], options)
+    .then(function(answer) {
+      if (answer === "Yes") cb()
+    })
 }
